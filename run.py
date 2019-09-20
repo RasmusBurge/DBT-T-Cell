3f9a4c974c7a6c30c396a4dbd6c3050545f6db38
RasmusBurge <55180555+RasmusBurge@users.noreply.github.com>
2019-09-20 12:57:46 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,34 +1,78 @@
+# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-from flask import Flask
-import os
 
-server = Flask(__name__)
-server.secret_key = os.environ.get('secret_key', 'secret')
-app = dash.Dash(name = __name__, server = server)
-app.config.supress_callback_exceptions = True
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello projekt 6!! LÖST'),
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-    dcc.Graph(
-        id='example-graph',    
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    ),
-    dcc.Input(id='my-id', value='initial value', type="text"),
-    html.Div(id='my-div')
+app = dash.Dash()
+
+# Bootstrap CSS.
+app.css.append_css({'external_url': 'https://codepen.io/amyoshino/pen/jzXypZ.css'})
+
+app.layout = html.Div([
+    html.Div([
+        html.Div([
+            html.H2(children='T-cells',
+                    className="nine columns",
+                    ),
+            html.Img(
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/SEM_Lymphocyte.jpg/1024px-SEM_Lymphocyte.jpg",
+                className='three columns',
+                style={
+                    'height': '10%',
+                    'width': '10%',
+                    'float': 'right',
+                    'position': 'relative',
+                    'margin-top': 10,
+                    'margin-right': 30
+                },
+            ),
+
+            html.Div(children='''DBT: Design Build Test
+                    ''',
+                    className='nine columns',
+            )
+        ], className='row', style={'backgroundColor': '#E0E0E0', 'margin-top': 0}),
+        html.Div(
+            [
+            html.Div([
+                dcc.Graph(
+                    id='example-graph',
+                    figure={
+                        'data': [
+                            {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                            {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                        ],
+                        'layout': {
+                            'title': 'Graph 1'
+                        }
+                    }
+                ),
+            ], className='six columns'
+            ),
+            html.Div([
+                dcc.Graph(
+                    id='example-graph-2',
+                    figure={
+                        'data': [
+                            {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'line', 'name': 'SF'},
+                            {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'line', 'name': u'Montréal'},
+                        ],
+                        'layout': {
+                            'title': 'Graph 2'
+                        }
+                    }
+                ),
+            ], className='six columns'
+            )
+        ], className='row'),
+        html.Div([
+            dcc.Input(id='my-id', value='initial value', type="text"),
+            html.Div(id='my-div')
+        ], className='row'),
+    ], className='ten columns offset-by-one')
+
 ])
 
 @app.callback(
@@ -37,3 +81,7 @@ app.layout = html.Div(children=[
 )
 def update_output_div(input_value):
     return 'You\'ve entered "{}"'.format(input_value)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
