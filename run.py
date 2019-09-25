28eaf76127dd64cbfa532a6bea5a45c1a9758652
LeoMonrro <leonardo-monrroy@hotmail.com>
2019-09-25 12:51:42 +0200

    försök

--- run.py
+++ run.py
@@ -1,43 +1,32 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-from flask import Flask
-import os
+import pandas as pd
+import plotly.graph_objects as go
 
-server = Flask(__name__)
-server.secret_key = os.environ.get('secret_key', 'secret')
-app = dash.Dash(name = __name__, server = server)
-app.config.supress_callback_exceptions = True
+
+app = dash.Dash()
+server = app.server
+df = pd.read_csv('https://raw.githubusercontent.com/LeoMonrroy/legendary-palm-tree/master/fakegraph.csv', sep=";")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello projekt 6!! LÖST'),
+    html.H1(children='Försök till visualiseing'),
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Grupp 6
     '''),
     dcc.Graph(
-        id='example-graph',
+        id='Försök till datavisualisering',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'line', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'line', 'name': u'Montréal'},
+                go.Scatter(
+                    x=df.Tidpunkter,
+                    y=df.testset,
+                    mode='lines'
+                )
             ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    ),
-    dcc.Input(id='my-id', value='initial value', type="text"),
-    html.Div(id='my-div')
-])
-
-human = pd.read_csv('.csv')
-df=human
-
-
-@app.callback(
-    Output(component_id='my-div', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
-)
-def update_output_div(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
+            'layout': go.Layout(
+                title='TH0 Genexpression över tid',
+                xaxis={'title' : 'Tid(h)'},
+                yaxis={'title': 'Expression .../...'},
+            )
+        })])
