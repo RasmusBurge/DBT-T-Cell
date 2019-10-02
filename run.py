866a32af46972f360369498008de01f3b449a15d
LeoMonrro <leonardo-monrroy@hotmail.com>
2019-10-02 11:23:09 +0200

    försök

--- run.py
+++ run.py
@@ -8,22 +8,23 @@ import plotly.graph_objects as go
 
 app = dash.Dash()
 server = app.server
-df = pd.read_csv('https://raw.githubusercontent.com/LeoMonrroy/legendary-palm-tree/master/fakegraph.csv', sep=";")
+df = pd.read_csv('https://github.com/yankarabelo/hello-world/raw/master/rawrdata.csv', sep=";")
+
 
 app.layout = html.Div(children=[
-    html.H1(children='Försök till visualiseing'),
+    html.H1(children='Försök till visualisering'),
     html.Div(children='''
-        Grupp 6
+        Grupp nr 6
     '''),
+
     dcc.Graph(
         id='Försök till datavisualisering',
         figure={
             'data': [
-                go.Scatter(
-                    x=df.Tidpunkter,
-                    y=df.testset,
-                    mode='lines'
-                )
+                go.Scatter(x=df.Tidpunkter, y=df.ENSG00000283297, name='ENSG00000283297', mode='lines+markers'),
+                go.Scatter(x=df.Tidpunkter, y=df.ENSG00000283573, name='ENSG00000283573 ', mode='lines+markers'),
+                go.Scatter(x=df.Tidpunkter, y=df.ENSG00000282961, name='ENSG00000282961 ', mode='lines+markers')
+
             ],
             'layout': go.Layout(
                 title='TH0 Genexpression över tid',
@@ -31,3 +32,5 @@ app.layout = html.Div(children=[
                 yaxis={'title': 'Expression .../...'},
             )
         })])
+
+
