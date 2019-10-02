7572feb27440f431176054faeb511279b3cf58b9
LeoMonrro <leonardo-monrroy@hotmail.com>
2019-10-02 11:21:05 +0200

    försök

--- run.py
+++ run.py
@@ -5,25 +5,18 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objects as go
 
+
 app = dash.Dash()
 server = app.server
-
-# Gets the data from Leo repo, using tool pd from panda
 df = pd.read_csv('https://raw.githubusercontent.com/LeoMonrroy/legendary-palm-tree/master/fakegraph.csv', sep=";")
 
 app.layout = html.Div(children=[
-
-    # Header
     html.H1(children='Försök till visualiseing'),
-
-    # New section, Body
     html.Div(children='''
         Grupp 6
     '''),
-
-    # Plotting the graph with the id; try_at_data_vis
     dcc.Graph(
-        id='try_at_data_vis',
+        id='Försök till datavisualisering',
         figure={
             'data': [
                 go.Scatter(
@@ -34,10 +27,7 @@ app.layout = html.Div(children=[
             ],
             'layout': go.Layout(
                 title='TH0 Genexpression över tid',
-                xaxis={'title': 'Tid(h)'},
+                xaxis={'title' : 'Tid(h)'},
                 yaxis={'title': 'Expression .../...'},
             )
-
         })])
-    # End of graph.
-
