ce9c962e9fb002363e1bef5c340ed63bc953bad7
Rasmus Burge <rasmus.burge.rb@gmail.com>
2019-10-02 11:00:38 +0200

    Added comments, changed graph id from swe to eng

--- run.py
+++ run.py
@@ -5,18 +5,25 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objects as go
 
-
 app = dash.Dash()
 server = app.server
+
+# Gets the data from Leo repo, using tool pd from panda
 df = pd.read_csv('https://raw.githubusercontent.com/LeoMonrroy/legendary-palm-tree/master/fakegraph.csv', sep=";")
 
 app.layout = html.Div(children=[
+
+    # Header
     html.H1(children='Försök till visualiseing'),
+
+    # New section, Body
     html.Div(children='''
         Grupp 6
     '''),
+
+    # Plotting the graph with the id; try_at_data_vis
     dcc.Graph(
-        id='Försök till datavisualisering',
+        id='try_at_data_vis',
         figure={
             'data': [
                 go.Scatter(
@@ -27,7 +34,10 @@ app.layout = html.Div(children=[
             ],
             'layout': go.Layout(
                 title='TH0 Genexpression över tid',
-                xaxis={'title' : 'Tid(h)'},
+                xaxis={'title': 'Tid(h)'},
                 yaxis={'title': 'Expression .../...'},
             )
+
         })])
+    # End of graph.
+
