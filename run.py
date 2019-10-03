c131dfb9127dd87eced9290754a9a080c6e85a44
LeoMonrro <leonardo-monrroy@hotmail.com>
2019-10-03 13:01:37 +0200

    Lagt till initial value "rutan"

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objects as go
-
+from dash.dependencies import Input, Output
 
 app = dash.Dash()
 server = app.server
@@ -35,7 +35,16 @@ app.layout = html.Div(children=[
                 xaxis={'title' : 'Tid(h)'},
                 yaxis={'title': 'Expression .../...'},
             )
-        })])
+        }),
+ dcc.Input(id='my-id', value='initial value', type="text"),
+    html.Div(id='my-div')
+])
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
